8eddc00a4521c589c84479230feb07c978168031
MY1HONEY <78687495+MY1HONEY@users.noreply.github.com>
2021-03-06 17:01:28 +0100

    Add files via upload

---
+++ Indexovani.py
@@ -0,0 +1,13 @@
+# Vytvoř prográmek, který využije indexingu tak, aby vytiskl a extrahoval:
+
+# Prvních 5 písmen slova 'indexování' a ulož je do proměnné prvnich_pet.
+prvnich_pet = "indexování"[:5]
+print(prvnich_pet)
+
+# Posledních 5 písmen slova 'indexování' a ulož je do proměnné poslednich_pet.
+poslednich_pet = "indexování"[-5:]
+print(poslednich_pet)
+
+# Laždé třetí písmeno slova 'indexování' a ulož je do proměnné kazde_treti.
+kazde_treti = "indexování"[::3]
+print(kazde_treti)
