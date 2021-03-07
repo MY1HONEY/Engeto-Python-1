824876f171a72eece0147f89560eda2ce11bc848
Tomas <78687495+MY1HONEY@users.noreply.github.com>
2021-03-07 13:30:53 +0100

    Update Vytvareni_listu__pridavani_prvku.py

--- Vytvareni_listu__pridavani_prvku.py
+++ Vytvareni_listu__pridavani_prvku.py
@@ -24,3 +24,27 @@ zamestnanci.insert(1, "Bruno")
 
 # Vytiskne obsah proměnné zamestnanci za string: 'Nová jména vložena do zamestnanci: '.
 print("Nova jmena vlozena za zamestnanci:",zamestnanci)
+
+# Odstraní prvek 'Bruno' z listu kandidati.
+kandidati.remove("Bruno")
+
+# Vytiskne obsah proměnné kandidati za string: 'Bruno odstraněn z kandidati: '.
+print("Bruno odstranen z kandidati:",kandidati)
+
+# Zopakovat jméno 'Anežka' uvnitř listu kandidati.
+kandidati = ["Anezka"]* 2
+
+# Vytisknout obsah listu kandidati za string: 'Opakování prvku Anežka v kandidati:'.
+print("Opakovani prvku Anezka v kandidati:",kandidati)
+
+# Spoj list zamestnanci s listem kandidati do nového listu, který zachová název zamestnanci.
+zamestnanci = zamestnanci + kandidati
+
+# Vytiskni obsah nového listu zamestnanci za string: 'Spojeni kandidati a zamestnanci: '.
+print("Spojeni kadidati a zamestnanci:",zamestnanci)
+
+# Vytisknout jméno na indexu 2 za string: 'Na indexu 2 je: '.
+print("Na indexu 2 je:",zamestnanci[2])
+
+# Vytisknout jméno na posledním indexu za string: 'Na <posledni_index> indexu je: '.
+print("Na poslednim indexu je:",zamestnanci[-1])
