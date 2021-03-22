e6762d535f25726c59566f1c54011e0ec01fd3c4
Tomas <78687495+MY1HONEY@users.noreply.github.com>
2021-03-22 21:57:02 +0100

    Add files via upload

---
+++ Razeni_podle_abecedy.py
@@ -0,0 +1,11 @@
+names = ['Michal', 'Pepa', 'Honza','Pavel', 'Lukas', 'Matej','Iva', 'Klara', 'Jana','Honza', 'Vasek','Milan', 'Michal']
+
+sorted_names = []
+
+while names:
+    i = names.pop()
+    sorted_names.append(i)
+    sorted_names.sort()
+
+print(sorted_names)
+    
