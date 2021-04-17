77ec1a14255b9e91d4d26ce14f49f6f36d8b5bc9
Tomas <78687495+MY1HONEY@users.noreply.github.com>
2021-04-17 23:45:45 +0200

    Update Formatovani_2.py

--- Formatovani_2.py
+++ Formatovani_2.py
@@ -1,26 +1,54 @@
+import random
+
 a = "+-------+"
 b = "| {} {} {} |"
 
-#1
+nahodne_cislo = random.randint(1,6)
 
-print(a)
-print((b).format(" "," "," "))
-print((b).format(" ","o"," "))
-print((b).format(" "," "," "))
-print(a)
+#1
+if nahodne_cislo == 1:
+    print(a)
+    print((b).format(" "," "," "))
+    print((b).format(" ","o"," "))
+    print((b).format(" "," "," "))
+    print(a)
 
 #2
-
-print(a)
-print((b).format("o"," "," "))
-print((b).format(" "," "," "))
-print((b).format(" "," ","o"))
-print(a)
+elif nahodne_cislo == 2:
+    print(a)
+    print((b).format("o"," "," "))
+    print((b).format(" "," "," "))
+    print((b).format(" "," ","o"))
+    print(a)
 
 #3
+elif nahodne_cislo == 3:
+    print(a)
+    print((b).format("o"," "," "))
+    print((b).format(" ","o"," "))
+    print((b).format(" "," ","o"))
+    print(a)
 
-print(a)
-print((b).format("o"," "," "))
-print((b).format(" ","o"," "))
-print((b).format(" "," ","o"))
-print(a)
+#4
+elif nahodne_cislo == 4:
+    print(a)
+    print((b).format("o"," ","o"))
+    print((b).format(" "," "," "))
+    print((b).format("o"," ","o"))
+    print(a)
+
+#5
+elif nahodne_cislo == 5:
+    print(a)
+    print((b).format("o"," ","o"))
+    print((b).format(" ","o"," "))
+    print((b).format("o"," ","o"))
+    print(a)
+
+#6
+else:
+    print(a)
+    print((b).format("o"," ","o"))
+    print((b).format("o"," ","o"))
+    print((b).format("o"," ","o"))
+    print(a)
